abd32b058700facf854d9d26d20e7cf1a5cdf11b
kdrai512 <138962964+kdrai512@users.noreply.github.com>
2026-07-22 18:39:19 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> containsAnagram;
+        unordered_map<string, vector<string>> ms;
+        for (int i = 0; i < strs.size(); i++) {
+            string word = strs[i];
+            string key = word;
+            sort(key.begin(), key.end());
+            ms[key].push_back(word);
+        }
+
+        for (auto& x : ms) {
+            containsAnagram.push_back(x.second);
+        }
+        return containsAnagram;
+    }
+};
